c0528a1d120125b6daf43de55b8fc79fc2c45d75
Felipe Muniz <lipe.baterra@gmail.com>
2023-03-11 21:09:12 -0300

    Criação do Contexto

---
+++ Infra/Configuracao/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Configuracao;
+
+public class AppDbContext : IdentityDbContext<ApplicationUser>
+{
+    public AppDbContext(DbContextOptions options ) : base( options ){}
+
+    #region Entidades
+    public DbSet<SistemaFinanceiro> SistemaFinanceiro { get; set; }
+    public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiro { get; set; }
+    public DbSet<Categoria> Categoria { get; set; }
+    public DbSet<Despesa> Despesa { get; set; }
+    #endregion
+
+    #region Metodos Override
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(ObterConnectionString());
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+        base.OnModelCreating(builder);
+    }
+    #endregion
+
+    #region Metodos Nativos
+    public string ObterConnectionString()
+    {
+        return "Data Source=DESKTOP-10DDISU;Initial Catalog=FinancialNineCore;Integrated Security=True;Pooling=False;Encrypt=False;TrustServerCertificate=False;";
+    }
+    #endregion    
+}
