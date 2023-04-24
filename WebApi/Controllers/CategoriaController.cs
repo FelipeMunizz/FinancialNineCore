a503bec9e0d37ef3170f6b840ea4d45c853dcb2c
Felipe Muniz <lipe.baterra@gmail.com>
2023-04-24 19:25:32 -0300

    Implementação controller categoria

--- WebApi/Controllers/CategoriaController.cs
+++ WebApi/Controllers/CategoriaController.cs
@@ -1,6 +1,6 @@
 ﻿using Domain.Interfaces.ICategoria;
 using Domain.Interfaces.InterfaceServicos;
-using Microsoft.AspNetCore.Http;
+using Entities.Entidades;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers;
@@ -16,5 +16,52 @@ public class CategoriaController : ControllerBase
     {
         _repository = repository;
         _service = service;
+    }
+    
+    [HttpGet("ObterCategoria/{id:int}")]
+    [Produces("application/json")]
+    public async Task<ActionResult<Categoria>> ObterCategoria(int id)
+    {
+        Categoria categoria = await _repository.GetEntityById(id);
+        return Ok(categoria);
+    }
+
+    [HttpGet("ListarCategoriasUsuario")]
+    [Produces("application/json")]
+    public async Task<object> ListarCategoriasUsuario(string email)
+    {
+        return await _repository.ListarCategoriasUsuario(email);
+    }
+
+    [HttpPost("AdicionarCategoria")]
+    [Produces("application/json")]
+    public async Task<object> AdicionarCategoria(Categoria categoria)
+    {
+        await _service.AdicionarCategoria(categoria);
+        return Ok(categoria);
+    }
+
+    [HttpPut("AtualizarCategoria")]
+    [Produces("application/json")]
+    public async Task<object> AtualizarCategoria(Categoria categoria)
+    {
+        await _service.AutalizarCategoria(categoria);
+        return Ok(categoria);
+    }
+
+    [HttpDelete("DeletarCategoria/{id:int}")]
+    [Produces("application/json")]
+    public async Task<IActionResult> DeletarCategoria(int id)
+    {
+        try
+        {
+            Categoria categoria = await _repository.GetEntityById(id);
+            await _repository.Delete(categoria);
+            return Ok(true); ;
+        }
+        catch (Exception)
+        {
+            return BadRequest(false);
+        }
     }
 }
