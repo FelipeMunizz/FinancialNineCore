f6de9f8a4b3a9b92ab18d4376ffbdfb27d2ce33b
Felipe Muniz <lipe.baterra@gmail.com>
2023-04-24 18:55:08 -0300

    controller categorias

--- WebApi/Controllers/CategoriaController.cs
+++ WebApi/Controllers/CategoriaController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Domain.Interfaces.ICategoria;
+using Domain.Interfaces.InterfaceServicos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApi.Controllers
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriaController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CategoriaController : ControllerBase
+    private readonly InterfaceCategoria _repository;
+    private readonly ICategoriaServico _service;
+
+    public CategoriaController(InterfaceCategoria repository, ICategoriaServico service)
     {
+        _repository = repository;
+        _service = service;
     }
 }
