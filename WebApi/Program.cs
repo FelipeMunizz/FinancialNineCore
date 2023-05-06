d43302a587150f848a5e9c59b70f096f97cf7cdc
Felipe Muniz <lipe.baterra@gmail.com>
2023-05-06 13:20:34 -0300

    Adicionando o Cors

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -112,6 +112,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+var devClient = "htpp://localhost:4200";
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().WithOrigins(devClient));
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
