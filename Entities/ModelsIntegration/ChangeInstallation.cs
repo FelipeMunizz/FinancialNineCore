e0643d9fc1a672b65db8561fb950f598c443f5eb
Felipe Muniz <78704793+FelipeMunizz@users.noreply.github.com>
2023-09-25 21:01:08 -0300

    Finalizado desenvolvimento de validacao do token

---
+++ Entities/ModelsIntegration/ChangeInstallation.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.ModelsIntegration;
+
+public class ChangeInstallation
+{
+    public string I_AMBIENTE { get; set; } = "PROD";
+    public string I_ANLAGE { get; set; }
+    public string I_BANDEIRA { get; set; } = "X";
+    public string I_CANAL { get; set; } = "ZINIT";
+    public string I_COBRADORA { get; set; } = string.Empty;
+    public string I_COD_SERV { get; set; } = "TC";
+    public string I_SERVICO { get; set; } = "A";
+    public string I_VERTRAG { get; set; }
+}
---
+++ Entities/ModelsIntegration/ReturnTokenValidate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.ModelsIntegration;
+
+public class ReturnTokenValidate
+{
+    public string access_token { get; set; }
+    public string expires_in { get; set; }
+    public string token_type { get; set; }
+    public string refresh_token { get; set; }
+    public string id_token { get; set; }
+    public string user_id { get; set; }
+    public string project_id { get; set; }
+}
